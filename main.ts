708ca53fa3f662d06fae06fea7ad251015b821c6
Tynasincredible <inesnmmiranda@gmail.com>
2025-11-29 20:38:12 +0000

    alterada amrkup do bloco setdate

--- main.ts
+++ main.ts
@@ -5,7 +5,15 @@ namespace MiaTime {
     let _month = 1
     let _year = 2025
 
-    //% block="setDate"
+
+    /**
+    * set Date
+    * @param day is the Day will be set, eg: 15
+    * @param month is the Month will be set, eg: 2
+    * @param year is the Year will be set, eg: 2018
+    */
+    //% blockId="MiaTime_SET_DATETIME" block="set day %day|month %month|year %year"
+    //% weight=60 blockGap
     //% day.min=1 day.max=31
     export function setDate(day: number, month: number, year: number) {
         _day = day;
